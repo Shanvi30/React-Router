f3aebb4e214f1041ae16f18370a8c0d622561273
Hanshika Shanvi <hshanvi30@gmail.com>
2025-12-10 21:43:37 +0530

    Final Commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,26 +2,26 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="py-20 bg-white">
+    <div className="py-55 bg-white">
       <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-          <div className="md:5/12 lg:w-5/12">
+          <div className="md:6/12 lg:w-5/12">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA1jMf7_JJEgBgQoe2cCzuCFcdpyVE9DRAcA&s"
               alt="image"
             />
           </div>
-          <div className="md:7/12 lg:w-6/12">
-            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+          <div className="md:6/12 lg:w-6/12">
+            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl text-center">
               React development is carried out by passionate developers
             </h2>
-            <p className="mt-6 text-gray-600">
+            <p className="mt-6 text-gray-600 text-center">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
               omnis voluptatem accusantium nemo perspiciatis delectus atque
               autem! Voluptatum tenetur beatae unde aperiam, repellat expedita
               consequatur! Officiis id consequatur atque doloremque!
             </p>
-            <p className="mt-4 text-gray-600">
+            <p className="mt-4 text-gray-600 text-center">
               Nobis minus voluptatibus pariatur dignissimos libero quaerat iure
               expedita at? Asperiores nemo possimus nesciunt dicta veniam
               aspernatur quam mollitia.
--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -8,7 +8,7 @@ function Github() {
         })
     }, [])
   return (
-    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+    <div className="py-39 text-center m-4 bg-gray-600 text-white p-4 text-3xl">
       Github Followers : {data.followers}
       <img src={data.avatar_url} alt="Git_pic" width={300}/>
     </div>
